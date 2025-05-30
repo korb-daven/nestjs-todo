76a141bb47b983a78ff3013c5a305f924a2cbc40
Korb Daven <korbdaven@gmail.com>
2025-05-30 09:41:41 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.enableCors({
+    origin: 'http://localhost:5173', // or use true to allow all origins
+  });
   await app.listen(process.env.PORT ?? 3100);
 }
 bootstrap();
